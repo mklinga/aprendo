dde842b0a244bded75fa6482864b13e88f65a0ab
Markus Klinga <laite@gmx.com>
2016-06-10 13:56:44 +0300

    Initial removal/cleaning of components

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -3,7 +3,7 @@ import Navbar from './navbar/navbar'
 import Hero from './hero/hero'
 import User from './user/user'
 
-let commonModule = angular.module('app.common', [
+const commonModule = angular.module('app.common', [
   Navbar.name,
   Hero.name,
   User.name
--- client/app/common/navbar/navbar.js
+++ client/app/common/navbar/navbar.js
@@ -2,7 +2,7 @@ import angular from 'angular'
 import uiRouter from 'angular-ui-router'
 import navbarComponent from './navbar.component'
 
-let navbarModule = angular.module('navbar', [
+const navbarModule = angular.module('navbar', [
   uiRouter
 ])
 
