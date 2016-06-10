02b1e8274d769045e8f6e49b579db478c5c8e4c1
Markus Klinga <laite@gmx.com>
2016-06-10 13:57:22 +0300

    Remove forgotten references to Hero component

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,11 +1,9 @@
 import angular from 'angular'
 import Navbar from './navbar/navbar'
-import Hero from './hero/hero'
 import User from './user/user'
 
 const commonModule = angular.module('app.common', [
   Navbar.name,
-  Hero.name,
   User.name
 ])
 
