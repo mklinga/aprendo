5be948ebf3008bee0d920e5028ef03097310e09d
Markus Klinga <laite@gmx.com>
2016-06-12 19:29:16 +0300

    Remove unnecessary user factory

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,8 +1,6 @@
 import angular from 'angular'
 import Navbar from './navbar/navbar'
-import User from './user/user'
 
 export default angular.module('app.common', [
-  Navbar.name,
-  User.name
+  Navbar.name
 ])
